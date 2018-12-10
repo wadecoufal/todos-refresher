16fcfaaaf2697b79d1073341dde35ad2589e8b6a
Wade Coufal <wade.coufal@gmail.com>
2018-12-09 16:11:33 -0800

    Create store, todos_actions and todos reducer

---
+++ frontend/reducers/todos_reducer.js
@@ -0,0 +1,39 @@
+import {
+  RECEIVE_TODOS, 
+  RECEIVE_TODO,
+} from '../actions/todo_actions';
+
+const initialState = {
+  1: {
+    id: 1,
+    title: "wash car",
+    body: "with soap",
+    done: false
+  },
+  2: {
+    id: 2,
+    title: "wash dog",
+    body: "with shampoo",
+    done: true
+  }
+};
+
+const todosReducer = (state = initialState, action) => {
+  Object.freeze(state);
+  let nextState = {};
+
+  switch(action.type) {
+    case RECEIVE_TODOS:
+      action.todos.forEach( todo => {
+        nextState[todo.id] = todo;
+      })
+    case RECEIVE_TODO:
+      const newTodo = {[action.todo.id]: action.todo};
+      return Object.assign({}, state, newTodo);
+    default:
+      return state;
+  }
+};
+
+
+export default todosReducer;
--- frontend/todo.jsx
+++ frontend/todo.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+import { receiveTodos, receiveTodo } from './actions/todo_actions';
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
+  const store = configureStore({});
+  console.log("Store", store);
+  window.receiveTodo = receiveTodo;
+  window.receiveTodos = receiveTodos;
+  window.store = store;
   ReactDOM.render(<h1>HELLO!</h1>, root);
 });
